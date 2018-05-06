e34684a81d00f87b0a9fedd01f7541b10a1a7de8
Yuki Fang <yfang4268@bths.edu>
2018-05-05 22:19:21 -0400

    can't display flags as there are no country flag emojis for the browser

--- Frankie.py
+++ Frankie.py
@@ -4,39 +4,27 @@ import emoji
 from emoji.unicode_codes import UNICODE_EMOJI
 
 MAIN_QS = ["What do you fancy today?", "What food are you feeling?"]
-emojis_list = [":pizza:",":icecream:", ":curry:", ":sushi:", ":spaghetti:", ":fries:", ":us:", ":jp:", ":it:", ":uk:", ":es:"]
+emojis_list = [":pizza:",":ice_cream:", ":sushi:", ":spaghetti:", ":french_fries:", ":Vietnam:", ":United_States:", ":Italy:",  ":Spain:", ":China:"]
 new_list = []
 for i in emojis_list:
     new_list.append(emoji.emojize(i))
-         
-def greeting():
-    sentence = input()
-    return "Hi there!"
-
-def extract_emoji():
-    print(sentence = input(random.choice(MAIN_QS + random.sample(new_list, 3))))
+print(new_list)         
+sentence = input()
+print("Hi there!")
+sentence = input(random.choice(MAIN_QS) + str(random.sample(new_list, 3)))
     
         
-    emoji_input = str(sentence)
-    
-    #gets unicode of emoji
-    unicode = 'U+{:X}'.format(ord(emoji_input))
-    
-    #gets text of emoji
-    text = UNICODE_EMOJI[emoji_input]
+emoji_input = str(sentence)
 
-    #saves text without colons
-    text_without_col = text[1:len(text)-1]
-    return text_without_col
+#gets unicode of emoji
+unicode = 'U+{:X}'.format(ord(emoji_input))
 
-def main():    
-    print(greeting())
-    print(extract_emoji())
-    
-main()
+#gets text of emoji
+text = UNICODE_EMOJI[emoji_input]
 
-'''next steps: take text_without_colon and use keyword to search yelp's categories, ask for location, then find best rated ones with
-most positive reviews'''
-#(emoji.emojize(":jp:"))
-#s = '😀'
+#saves text without colons
+text_without_col = text[1:len(text)-1]
+print(text_without_col)
+
+
 
