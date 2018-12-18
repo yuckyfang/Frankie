9942021e46e00ef2dbc31c6ec1262490bfea876d
Yuki Fang <yfang4268@bths.edu>
2018-12-17 23:09:01 -0500

    update from dataScience branch

--- Frankie.py
+++ Frankie.py
@@ -4,6 +4,7 @@ import random
 from random import choices 
 import emoji 
 from emoji.unicode_codes import UNICODE_EMOJI
+import geocoder
 import argparse, json, pprint, requests, sys, urllib
 from urllib.error import HTTPError
 from urllib.parse import quote
@@ -16,6 +17,7 @@ with open("config.json", "r") as f:
     data = json.load(f)
 API_KEY = data['api_key']
 API_HOST = data["API_HOST"]
+REVIEWS_PATH = data["REVIEWS_PATH"]
 SEARCH_PATH = data["SEARCH_PATH"]
 BUSINESS_PATH = data["BUSINESS_PATH"]
     
@@ -26,20 +28,41 @@ def request(host, path, api_key, url_params=None):
     headers = {
         'Authorization': 'Bearer %s' % api_key,
     }
-
-    response = requests.request('GET', url, headers=headers, params=url_params)    
+    
+    #need the following parameters (type dict) 
+#     params = {'name':'MinuteClinic', 'address1':'241 West 57th St', 'city':'New York', 'state':'NY', 'country':'US'}
+#     param_string = urllib.parse.urlencode(params)
+#     res = requests.request("GET", url, headers=headers, params = url_params)
+#     data = res.json()
+#     print(data)
+#     
+#     b_id = data['businesses'][0]['id'] 
+#     r_url = "/v3/businesses/" + b_id + "/reviews"    #review request URL creation based on business ID
+#     reviews = requests.request("GET",r_url,headers=headers, params = url_params)
+#     print(reviews.json())
+#     
+    response = requests.request('GET', url, headers=headers, params=url_params)   
     return response.json()
 
-
-def search(api_key, term, latitude, longitude):
+    
+def search(api_key, term, latitude, longitude, location=None):
     #Query the Search API by a search term and location.
-    url_params = {
-        'term': term.replace(' ', '+'),
-        'latitude': latitude, 
-        'longitude': longitude, 
-        'limit': SEARCH_LIMIT
-
-    }
+    if location is None:
+        url_params = {
+            'term': term.replace(' ', '+'),
+            'latitude': latitude, 
+            'longitude': longitude, 
+            'limit': SEARCH_LIMIT
+    
+        }
+    else:
+        url_params = {
+            'term': term.replace(' ', '+'),
+            'location': location, 
+            'limit': SEARCH_LIMIT
+    
+        }
+    
     return request(API_HOST, SEARCH_PATH, api_key, url_params=url_params)
 
 
@@ -49,10 +72,18 @@ def get_business(api_key, business_id):
     return request(API_HOST, business_path, api_key)
 
 
+def get_review(api_key, business_id):
+    review_path = BUSINESS_PATH + business_id + REVIEWS_PATH
+    return request(API_HOST, review_path, api_key)
+
+
 #Queries the API by the input values from the user.
-def query_api(term,latitude, longitude):
+def query_api(term,latitude, longitude, location=None):
     try: 
-        response = search(API_KEY, term, latitude, longitude)
+        if location is None:
+            response = search(API_KEY, term, latitude, longitude)
+        else:
+            response = search(API_KEY, term, None, None, location)
         businesses = response.get('businesses')
         if not businesses:
             print(u'No businesses for {0} in {1} found.'.format(term, location))
@@ -64,11 +95,54 @@ def query_api(term,latitude, longitude):
         print(u'Result for business "{0}" found:'.format(businesses[0]['name']))
         print(u'Result for business "{0}" found:'.format(businesses[1]['name']))
         print(u'Result for business "{0}" found:'.format(businesses[2]['name']))
-        pprint.pprint(response, indent=2)
+        #prints name, phone, location, hours, and rating of each business 
         print('')
-        pprint.pprint(response2, indent=2)
+        print(response['name'])
+        print(response['phone'])
+        pprint.pprint(response['location']['display_address'])
+        print(str(response['rating']) + ' stars')
+        #prints 3 reviews of each business 
+        review = get_review(API_KEY, businesses[0]['id'])
+        pprint.pprint(review['reviews'][0]['rating'])
+        pprint.pprint(review['reviews'][0]['text'])
+        pprint.pprint(review['reviews'][1]['rating'])
+        pprint.pprint(review['reviews'][1]['text'])
+        pprint.pprint(review['reviews'][2]['rating'])
+        pprint.pprint(review['reviews'][2]['text'])
+        pprint.pprint(response['hours'])
         print('')
-        pprint.pprint(response3, indent=2)
+        print(response2['name'])
+        print(response2['phone'])
+        pprint.pprint(response2['location']['display_address'])
+        print(str(response2['rating']) + ' stars')
+        review2 = get_review(API_KEY, businesses[1]['id'])
+        pprint.pprint(review2['reviews'][0]['rating'])
+        pprint.pprint(review2['reviews'][0]['text'])
+        pprint.pprint(review2['reviews'][1]['rating'])
+        pprint.pprint(review2['reviews'][1]['text'])
+        pprint.pprint(review2['reviews'][2]['rating'])
+        pprint.pprint(review2['reviews'][2]['text'])
+        pprint.pprint(response2['hours'])
+        print('')
+        print(response3['name'])
+        print(response3['phone'])
+        pprint.pprint(response3['location']['display_address'])
+        print(str(response3['rating']) + ' stars')
+        review3 = get_review(API_KEY, businesses[2]['id'])
+        pprint.pprint(review3['reviews'][0]['rating'])
+        pprint.pprint(review3['reviews'][0]['text'])
+        pprint.pprint(review3['reviews'][1]['rating'])
+        pprint.pprint(review3['reviews'][1]['text'])
+        pprint.pprint(review3['reviews'][2]['rating'])
+        pprint.pprint(review3['reviews'][2]['text'])
+        pprint.pprint(response3['hours'])
+
+
+#         pprint.pprint(response, indent=2)
+#         print('')
+#         pprint.pprint(response2, indent=2)
+#         print('')
+#         pprint.pprint(response3, indent=2)
     except HTTPError as error:
         sys.exit(
             'Encountered HTTP error {0} on {1}:\n {2}\nAbort program.'.format(
@@ -79,7 +153,16 @@ def query_api(term,latitude, longitude):
             )
         
         
-
+def get_location(term):
+    LOCATION = input("location?")
+    locations = ['near me', 'current location', 'here', 'around me', 'around here', 'current', 'me', 'right here']
+    if LOCATION.lower() in locations: 
+        g = geocoder.ip('me')
+        query_api(term,g.latlng[0], g.latlng[1])
+    else: 
+        query_api(term, None, None, LOCATION)
+        
+          
 def ask_for_emoji():
     MAIN_QS = ["What do you fancy today?", "What food are you feeling?"]
     emojis_list = [":pizza:",":ice_cream:", ":sushi:", ":spaghetti:", ":sake:", ":french_fries:", ":curry_rice:", ":doughnut:", ":Vietnam:", ":United_States:", ":Italy:", ":China:", ":United_Kingdom:", ":Chile:", ":Egypt:"]
@@ -95,28 +178,13 @@ def ask_for_emoji():
         emoji_input = str(sentence)
         #gets text of emoji
         text = UNICODE_EMOJI[emoji_input]
-        #saves text without colons
+        #saves text without colons aka search term
         TERM = text[1:len(text)-1]
-        LOCATION = input("location?")
-        locations = ['near me', 'current location', 'here', 'around me', 'around here', 'current']
-        if LOCATION.lower() in locations: 
-            import geocoder
-            g = geocoder.ip('me')
-            #only finds your city and state...try latlng
-            query_api(TERM,g.latlng[0], g.latlng[1])
-        else: 
-            query_api(TERM, LOCATION)       
+        get_location(TERM)
+             
     else:
-        print("Sorry, I only accept emojis" + ' ' + emoji.emojize(':crying_face:'))
+        print("Sorry, I can only read emojis" + ' ' + emoji.emojize(':crying_face:'))
         ask_for_emoji()
-
-
+    
 ask_for_emoji()
 
-
-'''emoji chart 
-https://unicode.org/emoji/charts/full-emoji-list.html '''
-
-'''import geocoder
-g = geocoder.ip('me')
-print(g.latlng)'''
